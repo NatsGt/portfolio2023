41ddab73c6869da7d5bdfdfb6f878b5d9e26f22c
natsgt <nataliarodas7858@gmail.com>
2023-03-03 17:25:47 +0100

    create about section

---
+++ components/landing/About.tsx
@@ -0,0 +1,19 @@
+import PageContainer from "../PageContainer"
+import styles from "@/styles/components/about.module.scss"
+
+type AboutProps = {
+  description: string
+}
+
+const About: React.FC<AboutProps> = ({ description }) => {
+  return (
+    <PageContainer background="light" id="about">
+      <div className={`${styles.about} basicDescription`}>
+        <h2 className="basicSubtitle">About</h2>
+        <p>{description}</p>
+      </div>
+    </PageContainer>
+  )
+}
+
+export default About

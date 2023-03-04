0d15e744350856c9a4ad7a0f754be11e367f8c68
natsgt <nataliarodas7858@gmail.com>
2023-03-04 22:30:55 +0100

    add new component for icon

--- components/landing/Skills.tsx
+++ components/landing/Skills.tsx
@@ -1,17 +1,8 @@
-import { Skill, Skills } from "@/utils/aboutMe"
-import { IconHtml5 } from "../icons/icons"
-import { IconCss3 } from "../icons/icons"
-import { IconJavascript } from "../icons/icons"
-import { IconTypescript } from "../icons/icons"
-import { IconReact } from "../icons/icons"
-import { IconGithub } from "../icons/icons"
-import { IconNextjs } from "../icons/icons"
-import { IconSass } from "../icons/icons"
-import { IconBxlPostgresql } from "../icons/icons"
-import { IconDatadog } from "../icons/icons"
+import { Skills } from "@/utils/aboutMe"
 import { Icon } from "../icons"
 import styles from "../../styles/components/skills.module.scss"
 import PageContainer from "../PageContainer"
+import { skillIcon } from "@/utils/icons"
 
 type SkillsProps = {
   description: string
@@ -19,30 +10,6 @@ type SkillsProps = {
 }
 
 const Skills: React.FC<SkillsProps> = ({ description, skills }) => {
-  const skillIcon = (skill: string): React.ElementType => {
-    switch (skill) {
-      case Skill.HTML:
-        return IconHtml5
-      case Skill.CSS:
-        return IconCss3
-      case Skill.JAVASCRIPT:
-        return IconJavascript
-      case Skill.TYPESCRIPT:
-        return IconTypescript
-      case Skill.REACT:
-        return IconReact
-      case Skill.GITHUB:
-        return IconGithub
-      case Skill.NEXT:
-        return IconNextjs
-      case Skill.SASS:
-        return IconSass
-      case Skill.SQL:
-        return IconBxlPostgresql
-      default:
-        return IconDatadog
-    }
-  }
   return (
     <PageContainer id="skills" background="dark">
       <div className={styles.skills}>

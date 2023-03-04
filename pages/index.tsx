5b5bb61315ec993b0c3a9840046d0171e8d63a51
natsgt <nataliarodas7858@gmail.com>
2023-03-04 22:31:15 +0100

    add contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import FrontPage from "@/components/landing/Frontpage"
 import data from "@/utils/aboutMe"
 import Skills from "@/components/landing/Skills"
 import About from "@/components/landing/About"
+import Contact from "@/components/landing/Contact"
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <FrontPage description={data.briefDescription} contact={data.contact} />
         <About description={data.aboutMe} />
         <Skills description={data.skillsDescription} skills={data.skills} />
+        <Contact contacts={data.contact} description={data.contactSummary} />
       </main>
     </>
   )

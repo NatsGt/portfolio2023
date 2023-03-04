eada42c33f7b8db71ba66115012f9725355cc077
natsgt <nataliarodas7858@gmail.com>
2023-03-04 22:30:32 +0100

    add new components for icons and contact icon

--- components/landing/Frontpage.tsx
+++ components/landing/Frontpage.tsx
@@ -1,9 +1,7 @@
-import { ContactName, Contacts } from "@/utils/aboutMe"
+import { Contacts } from "@/utils/aboutMe"
 import styles from "../../styles/components/frontpage.module.scss"
-import { Icon } from "../icons"
-import { IconDatadog, IconGithub, IconLinkedin, IconMail } from "../icons/icons"
-import Link from "next/link"
 import PageContainer from "../PageContainer"
+import ContactIcon from "../ContactIcon"
 
 type FrontPageProps = {
   description: string
@@ -11,32 +9,12 @@ type FrontPageProps = {
 }
 
 const FrontPage: React.FC<FrontPageProps> = ({ description, contact }) => {
-  const contactIcon = (name: string): React.ElementType => {
-    switch (name) {
-      case ContactName.LINKEDIN:
-        return IconLinkedin
-      case ContactName.GITHUB:
-        return IconGithub
-      case ContactName.MAIL:
-        return IconMail
-      default:
-        return IconDatadog
-    }
-  }
-
   return (
     <PageContainer id="home" background="dark">
       <div className={styles.frontpage}>
         <div className={styles.contactIcons}>
           {contact.map((item) => (
-            <Link
-              href={item.value}
-              target="_blank"
-              key={item.id}
-              className={styles.contact}
-            >
-              <Icon icon={contactIcon(item.name)} />
-            </Link>
+            <ContactIcon item={item} key={item.id} />
           ))}
         </div>
         <h1>Natalia Rodas</h1>

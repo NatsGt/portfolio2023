1a057064f2ed0f68fb014447820de5bf6a248212
natsgt <nataliarodas7858@gmail.com>
2023-04-03 16:01:32 +0200

    add real data

--- components/landing/Project/index.tsx
+++ components/landing/Project/index.tsx
@@ -1,11 +1,16 @@
 import { Icon } from "@/components/icons"
-import { Skill } from "@/utils/aboutMe"
+import { Project, Skill } from "@/utils/aboutMe"
 import { skillIcon } from "@/utils/icons"
 import Image from "next/image"
 import styles from "../../../styles/components/projectcard.module.scss"
-import { IconWeb } from "@/components/icons/icons"
+import { IconGithub, IconWeb } from "@/components/icons/icons"
+import { capitalize } from "@/utils/string"
 
-const ProjectCard = () => {
+interface ProjectProp {
+  data: Project
+}
+
+const ProjectCard: React.FC<ProjectProp> = ({ data }) => {
   return (
     <div className={styles.cardContainer}>
       <div className={styles.imageContainer}>
@@ -18,22 +23,32 @@ const ProjectCard = () => {
       </div>
       <div className={styles.projectData}>
         <div className={styles.projectData__text}>
-          <h3>Project title</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Vitae ultrices mauris
-            consectetur cursus. Nullam ullamcorper ante fringilla sapien. Vitae
-            egestas at scelerisque gravida.
-          </p>
+          <h3>{data.title}</h3>
+          <p>{data.description}</p>
           <div className={styles.link}>
-            <Icon icon={IconWeb} />
-            <a>www.google.com</a>
+            {data.links.map((link) => {
+              return (
+                <div key={link.id} className={styles.oneLink}>
+                  <Icon icon={link.type === "url" ? IconWeb : IconGithub} />
+                  <a href={link.link} target="_blank">
+                    {link.type === "url" ? "Website" : "Repository"}
+                  </a>
+                </div>
+              )
+            })}
           </div>
         </div>
         <div className={styles.technologies}>
           <h4>Technologies used</h4>
-          <div className={styles.technology}>
-            <Icon icon={skillIcon(Skill.REACT)} color="white" />
-            React
+          <div className={styles.list}>
+            {data.technologies.map((item, index) => {
+              return (
+                <div key={index} className={styles.technology}>
+                  <Icon icon={skillIcon(item)} color="white" />
+                  {capitalize(item)}
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>

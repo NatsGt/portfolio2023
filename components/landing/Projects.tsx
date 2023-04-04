1c9ff72d3ef119ab5d5c84cb8326d8234d4e5926
natsgt <nataliarodas7858@gmail.com>
2023-04-04 12:21:55 +0200

    responsive style

--- components/landing/Projects.tsx
+++ components/landing/Projects.tsx
@@ -11,9 +11,9 @@ interface ProjectsProps {
 const Projects: React.FC<ProjectsProps> = ({ data }) => {
   return (
     <PageContainer background="light" id="projects">
-      <div>
+      <div className={styles.projectContainer}>
         <h2 className="basicSubtitle">Projects</h2>
-        <div className={styles.projectContainer}>
+        <div className={styles.projectContainer__projects}>
           <Carousel>
             {data.map((item, index) => (
               <ProjectCard data={item} key={index} />
